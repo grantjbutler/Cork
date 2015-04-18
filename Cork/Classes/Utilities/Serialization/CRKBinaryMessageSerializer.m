91df96c5bce853ce7717b46add879024ac4d5be9
Grant J. Butler <butler.grant.j@gmail.com>
2015-04-18 18:55:04 -0400

    Implement serialization.

--- Cork/Classes/Utilities/Serialization/CRKBinaryMessageSerializer.m
+++ Cork/Classes/Utilities/Serialization/CRKBinaryMessageSerializer.m
@@ -8,12 +8,33 @@
 
 #import "CRKBinaryMessageSerializer.h"
 
+#include "crc32.h"
+
 @implementation CRKBinaryMessageSerializer
+
+// Format:
+// [ crc32 ][  sender  ][ recipient ][ date-sent ][ message-length ][ message ]
+//  4 bytes   16 bytes     16 bytes     4 bytes        4 bytes        variable
 
 - (NSData *)serializedDataForMessage:(id<CRKMessage>)message {
     NSMutableData *serializedData = [NSMutableData data];
     
+    NSString *senderString = message.sender.UUIDString;
+    NSString *recipientString = message.recipient.UUIDString;
+    NSInteger sentTimestamp = floor([message.dateSent timeIntervalSince1970]);
+    NSString *messageText = message.message;
     
+    [serializedData appendData:[senderString dataUsingEncoding:NSUTF8StringEncoding]];
+    [serializedData appendData:[recipientString dataUsingEncoding:NSUTF8StringEncoding]];
+    [serializedData appendBytes:&sentTimestamp length:sizeof(sentTimestamp)];
+    
+    NSData *messageData = [messageText dataUsingEncoding:NSUTF8StringEncoding];;
+    NSUInteger messageLength = messageData.length;
+    [serializedData appendBytes:&messageLength length:sizeof(messageLength)];
+    [serializedData appendData:messageData];
+    
+    uint32_t crcHash = crc32(0, messageData.bytes, messageData.length);
+    [serializedData replaceBytesInRange:NSMakeRange(0, 0) withBytes:&crcHash length:sizeof(crcHash)];
     
     return serializedData;
 }
