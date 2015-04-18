55d5e815a9507c1fe509c0d689c133c2026f6d3b
Grant J. Butler <butler.grant.j@gmail.com>
2015-04-18 18:23:09 -0400

    Add CRKBinaryMessageSerializer.

---
+++ Cork/Classes/Utilities/Serialization/CRKBinaryMessageSerializer.h
@@ -0,0 +1,13 @@
+//
+//  CRKBinayMessageSerializer.h
+//  Cork
+//
+//  Created by Grant Butler on 4/18/15.
+//  Copyright (c) 2015 Grant Butler. All rights reserved.
+//
+
+#import "CRKMessageSerializer.h"
+
+@interface CRKBinaryMessageSerializer : NSObject <CRKMessageSerializer>
+
+@end
---
+++ Cork/Classes/Utilities/Serialization/CRKBinaryMessageSerializer.m
@@ -0,0 +1,21 @@
+//
+//  CRKBinayMessageSerializer.m
+//  Cork
+//
+//  Created by Grant Butler on 4/18/15.
+//  Copyright (c) 2015 Grant Butler. All rights reserved.
+//
+
+#import "CRKBinaryMessageSerializer.h"
+
+@implementation CRKBinaryMessageSerializer
+
+- (NSData *)serializedDataForMessage:(id<CRKMessage>)message {
+    NSMutableData *serializedData = [NSMutableData data];
+    
+    
+    
+    return serializedData;
+}
+
+@end
