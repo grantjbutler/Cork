3d4c8cfd1e359b39c7bc20ec7023b622daad0553
Grant J. Butler <butler.grant.j@gmail.com>
2015-04-18 13:43:08 -0400

    Add base xcode project.

---
+++ Cork/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Cork
+//
+//  Created by Grant Butler on 4/18/15.
+//  Copyright (c) 2015 Grant Butler. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Cork/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Cork
+//
+//  Created by Grant Butler on 4/18/15.
+//  Copyright (c) 2015 Grant Butler. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
