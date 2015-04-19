19414d61cb352e7c530d8af4beea76daa87c1d34
Grant J. Butler <butler.grant.j@gmail.com>
2015-04-18 20:42:55 -0400

    Add CRKPeripheral.

---
+++ Cork/Classes/Model/Core Data/CRKPeripheral.h
@@ -0,0 +1,13 @@
+//
+//  CRKPeripheral.h
+//  Scavenger
+//
+//  Created by Grant Butler on 04/18/15.
+//  Copyright (c) 2015 Grant Butler. All rights reserved.
+//
+
+#import "_CRKPeripheral.h"
+
+@interface CRKPeripheral : _CRKPeripheral
+
+@end
---
+++ Cork/Classes/Model/Core Data/Generated/_CRKPeripheral.h
@@ -0,0 +1,21 @@
+//
+// _CRKPeripheral.h
+// Scavenger
+//
+// Copyright (c) 2015 Grant Butler. All rights reserved.
+//
+
+// DO NOT EDIT. This file is machine-generated and constantly overwritten.
+// Make changes to CRKPeripheral.h instead.
+
+#import <CoreData/CoreData.h>
+
+@interface _CRKPeripheral : NSManagedObject
+
+@property (nonatomic, copy) NSString* id;
+
+@end
+
+@interface _CRKPeripheral (_CRKPeripheralCoreDataGeneratedAccessors)
+
+@end
