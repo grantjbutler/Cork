d276857a75272ce89c8b2d7e766e28eeaacd44a7
Michael Selsky <mselsky@ispeech.org>
2015-04-19 09:18:07 -0400

    Moved addBorder to an initializer

--- Cork/CRKFloatLabeledTextField.m
+++ Cork/CRKFloatLabeledTextField.m
@@ -10,10 +10,29 @@
 
 @implementation CRKFloatLabeledTextField
 
+- (instancetype)init{
+    if (self = [super init]) {
+        [self addBorder];
+    }
+    return self;
+}
 
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect {
+- (instancetype)initWithCoder:(NSCoder *)aDecoder{
+    if (self = [super initWithCoder:aDecoder]){
+        [self addBorder];
+    }
+    return self;
+}
+
+- (instancetype)initWithFrame:(CGRect)frame{
+    if (self = [super initWithFrame:frame]){
+        [self addBorder];
+    }
+    
+    return self;
+}
+
+- (void)addBorder{
     CALayer *upperBorder = [CALayer layer];
     upperBorder.backgroundColor = [[UIColor groupTableViewBackgroundColor] CGColor];
     upperBorder.frame = CGRectMake(0, 0, CGRectGetWidth(self.frame), 1.0f);
