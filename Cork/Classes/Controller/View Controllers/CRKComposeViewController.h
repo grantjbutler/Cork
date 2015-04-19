339d41109b8546c50072e70a05f0a76cd75b9e88
Michael Selsky <mselsky@ispeech.org>
2015-04-19 00:06:24 -0400

    Added JVFloatLabeledTextField

---
+++ Cork/Classes/Controller/View Controllers/CRKComposeViewController.h
@@ -0,0 +1,13 @@
+//
+//  CRKComposeViewController.h
+//  Cork
+//
+//  Created by MichaelSelsky on 4/19/15.
+//  Copyright (c) 2015 Grant Butler. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CRKComposeViewController : UIViewController
+
+@end
---
+++ Pods/Target Support Files/Pods-Cork-JVFloatLabeledTextField/Pods-Cork-JVFloatLabeledTextField-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Cork_JVFloatLabeledTextField : NSObject
+@end
+@implementation PodsDummy_Pods_Cork_JVFloatLabeledTextField
+@end
