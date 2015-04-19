e3d798dec06f00b010feb166a5be23ef0ee4535c
Grant J. Butler <butler.grant.j@gmail.com>
2015-04-19 00:07:17 -0400

    Show a notification when we get a message.

--- Cork/Classes/Controller/Application/CRKAppDelegate.m
+++ Cork/Classes/Controller/Application/CRKAppDelegate.m
@@ -10,7 +10,6 @@
 
 #import "CRKPeripheralController.h"
 #import "CRKBluetoothCentralController.h"
-#import <CocoaLumberjack/CocoaLumberjack.h>
 #import "CRKCoreDataHelper.h"
 
 #import "CRKMessageProtocol.h"
@@ -19,6 +18,13 @@
 #import "CRKBinaryMessageDeserializer.h"
 
 #import "CRKUser.h"
+#import "CRKMessage.h"
+#import "CRKPeripheral.h"
+
+#import "NSManagedObject+CRKAdditions.h"
+
+#import <CocoaLumberjack/CocoaLumberjack.h>
+#import <NSHash/NSString+NSHash.h>
 
 @interface CRKAppDelegate () <CRKBluetoothCentralControllerDelegate, CRKPeripheralControllerDelegate>
 
@@ -48,13 +54,61 @@
 }
 
 - (void)controller:(CRKPeripheralController *)controller didReceiveMessage:(id<CRKMessage>)message {
-    NSLog(@"Did receive: %@", message);
+    CRKMessage *coreDataMessage = message;
+    NSManagedObjectContext *context = coreDataMessage.managedObjectContext;
+    [context performBlock:^{
+        NSError *saveError;
+        if (![context save:&saveError]) {
+            NSLog(@"Error saving: %@", saveError);
+            
+            return;
+        }
+        
+        [[CRKCoreDataHelper sharedHelper].persistenceController saveContextAndWait:NO completion:nil];
+    }];
+    
+    UIAlertController *alertController = [UIAlertController alertControllerWithTitle:@"Got Message" message:message.text preferredStyle:UIAlertControllerStyleAlert];
+    [alertController addAction:[UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) {
+        [self.window.rootViewController dismissViewControllerAnimated:YES completion:nil];
+    }]];
+    [self.window.rootViewController presentViewController:alertController animated:YES completion:nil];
 }
 
 - (id<CRKMessage>)controller:(CRKBluetoothCentralController *)controller messageToTransmitToPeripheral:(CBPeripheral *)peripheral {
-    // TODO: Hook this up to Core Data.
+    NSManagedObjectContext *context = [[CRKCoreDataHelper sharedHelper].persistenceController newPrivateChildManagedObjectContext];
     
-    return nil;
+    NSString *hashedDeviceIdentifier = peripheral.identifier.UUIDString.SHA1;
+    CRKPeripheral *coreDataPeripheral = [CRKPeripheral uniqueObjectWithIdentifier:hashedDeviceIdentifier inContext:context];
+    
+    CRKUser *currentUser = [CRKUser currentUserInContext:context];
+    
+    NSFetchRequest *fetchRequest = [NSFetchRequest fetchRequestWithEntityName:[CRKMessage entityName]];
+    fetchRequest.predicate = [NSPredicate predicateWithFormat:@"%K != %@ AND %K > 0 AND %@ NOT IN %K", @"receiver", currentUser, @"timeToLive", coreDataPeripheral, @"peripherals"];
+    fetchRequest.fetchLimit = 1;
+    
+    NSError *fetchError;
+    NSArray *results = [context executeFetchRequest:fetchRequest error:&fetchError];
+    if (!results) {
+        NSLog(@"Error fetching: %@", fetchError);
+        
+        return nil;
+    }
+    
+    CRKMessage *message = results.firstObject;
+    [message addPeripheralsObject:coreDataPeripheral];
+    
+    [context performBlock:^{
+        NSError *saveError;
+        if (![context save:&saveError]) {
+            NSLog(@"Error saving: %@", saveError);
+            
+            return;
+        }
+        
+        [[CRKCoreDataHelper sharedHelper].persistenceController saveContextAndWait:NO completion:nil];
+    }];
+    
+    return message;
 }
 
 @end
