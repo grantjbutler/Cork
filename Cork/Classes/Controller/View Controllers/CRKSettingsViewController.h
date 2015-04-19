1bd61aae348c0712351849cceb4f3469bf41171c
Grant J. Butler <butler.grant.j@gmail.com>
2015-04-19 09:43:35 -0400

    Add basic CRKSettingsViewController.h

---
+++ Cork/Classes/Controller/View Controllers/CRKSettingsViewController.h
@@ -0,0 +1,13 @@
+//
+//  CRKSettingsViewController.h
+//  Cork
+//
+//  Created by Grant Butler on 4/19/15.
+//  Copyright (c) 2015 Grant Butler. All rights reserved.
+//
+
+#import <JMStaticContentTableViewController/JMStaticContentTableViewController.h>
+
+@interface CRKSettingsViewController : JMStaticContentTableViewController
+
+@end
